2ad936dd35a4334c3bbdad1bad827c615ed27b33
Kim In Su <insu9058@naver.com>
2024-01-31 23:47:51 +0900

    스트링 타입 함수 공부함

---
+++ jsaio/stringFunctions.js
@@ -0,0 +1,22 @@
+// Using single quotes
+let firstName = "barney";
+
+// Using double quotes
+let lastName = "stinson";
+
+// Using backticks
+let msg = `My name is ${firstName} ${lastName}`;
+
+firstName.charAt(0); // `b`
+
+firstName.concat(` `, lastName, ` !`); // `barney stinson !`
+
+firstName.indexOf(`b`); // 0
+firstName.indexOf(`k`); // -1
+
+firstName.subString(0, 1); // `b`
+
+firstName[0]; // `b`
+firstName[-1]; // undefined
+
+firstName.replace(`b`, `B`); // `Barney`
